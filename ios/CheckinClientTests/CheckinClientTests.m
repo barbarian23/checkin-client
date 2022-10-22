ffa55f355ade11a4b55d60cf5fe53d61d6a91e00
trangdth <trangdth@elifetech.vn>
2022-10-23 00:19:52 +0700

    feat: change name project

--- ios/CheckinClientTests/CheckinClientTests.m
+++ ios/CheckinClientTests/CheckinClientTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TestSmartLockTests : XCTestCase
+@interface CheckinClientTests : XCTestCase
 
 @end
 
-@implementation TestSmartLockTests
+@implementation CheckinClientTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
