faec25530a53fd0080c33a67bdd701de437e71e8
nangbanmai1907 <dothihuyentrang1907@gmail.com>
2022-12-26 06:32:13 +0700

    Update 26_12

--- src/utils/services/api/Constants.ts
+++ src/utils/services/api/Constants.ts
@@ -1,9 +1,9 @@
 // Product VN
-// export const BRANCH_CODE = '2e111519-358d-442f-93fd-159cd4e7d5d7';
-// export const TEN_ANT = 'NIKNAILHUB';
-// export const HOST = 'server.6figurespos.com';
-// export const NAME = '';
-// export const PATH = '/api/pos/public/api-client/';
+export const BRANCH_CODE = '2e111519-358d-442f-93fd-159cd4e7d5d7';
+export const TEN_ANT = 'NIKNAILHUB';
+export const HOST = 'server.6figurespos.com';
+export const NAME = 'Nik Nail Hub';
+export const PATH = '/api/pos/public/api-client/';
 
 // Product My
 // export const BRANCH_CODE = '3abe1bf2-3b51-4cb3-b034-000f3b43b9e6';
@@ -13,11 +13,11 @@
 // export const PATH = '/api/pos/public/api-client/';
 
 // Product My - 2
-export const BRANCH_CODE = '3489c4f6-c7c2-4abc-b74d-af53a39996ab';
-export const TEN_ANT = 'billionnailandbeautybar';
-export const HOST = 'dev-server.6figurespos.com';
-export const NAME = 'Billion Nail and Beauty Bar';
-export const PATH = '/api/pos/nail/public/api-client/';
+// export const BRANCH_CODE = '3489c4f6-c7c2-4abc-b74d-af53a39996ab';
+// export const TEN_ANT = 'billionnailandbeautybar';
+// export const HOST = 'dev-server.6figurespos.com';
+// export const NAME = 'Billion Nail and Beauty Bar';
+// export const PATH = '/api/pos/nail/public/api-client/';
 
 // DEV
 // export const BRANCH_CODE = '10e0633b-9f1c-438e-95e8-a86a7a2499fc';
